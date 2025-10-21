3e6551a8ee36935786fa71ad1f42b9cce865c34a
Santiago <gironlozano1975@gmail.com>
2025-10-21 17:55:05 -0500

    Create User Model, and Create Register and Login forms

---
+++ app/forms/login.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField 
+from wtforms.validators import DataRequired
+
+class Login(FlaskForm):
+    user_name = StringField('User Name: ', validators=DataRequired())
+    password = PasswordField('Password', validators=DataRequired())
+    button = SubmitField('Login')
---
+++ app/models/user.py
@@ -0,0 +1,25 @@
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+from app.__init__ import bd
+
+encryptor = Bcrypt()
+
+class User(bd.Model, UserMixin):
+    id = bd.Column(bd.Integer, primary_key=True)
+    name = bd.Column(bd.String(64), nullable=False)
+    last_name = bd.Column(bd.String(64), nullable=False)
+    user_name = bd.Column(bd.String(64), nullable=False, unique=True)
+    age = bd.Column(bd.Integer, nullable=False)
+    email = bd.Colmn(bd.String(64), unique=True)
+    password = bd.Column(bd.String(218))
+    
+    def __init__(self, name, last_name, age, email, password):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.password = encryptor.generate_password_hash(password.encode('utf-8'))
+        
+    def password_validator(self, password):
+        return encryptor.check_password_hash(self.password, password)
+            
